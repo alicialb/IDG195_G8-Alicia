dcf97fc404b6edf0db6ac0c3e1426a86b5ecc731
Alicia López <alicialopez@Alices-MacBook-Pro.local>
2022-02-18 21:35:44 -0800

    Suma Program Addition

---
+++ operaciones copy.py
@@ -0,0 +1,24 @@
+#Operaciones
+
+num1 = input ("Inserte un número: ")
+num2 = input ("Inserte otro número: ")
+
+suma = num1 + num2
+print ("El total de la suma es: ", suma)
+
+resta = num1 - num2
+print ("El total de la resta es: ", resta)
+
+multiplicacion = num1 * num2
+print ("El total de la multiplicacion es: ", multiplicacion)
+
+division = num1 / num2
+print ("El total de la division es: ", division)
+
+#Operaciones 2
+
+suma = 10 + 10 + 10
+resta = 10 - 10 - 10
+multiplicacion = 10 * 10 * 10
+division = 10 / 10 / 10
+print("Suma: " , suma ,  "Resta: " , resta , "Multiplicacion: " , multiplicacion , "Division: " , division)
